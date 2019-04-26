9d56c7ef154a80fe31e0767f108c60837cf08dde
Korbinian Breu <korbinian.breu@payworksmobile.com>
2019-04-26 10:48:13 +0200

    new ver

--- app/src/main/java/com/example/pax_beta/MainActivity.java
+++ app/src/main/java/com/example/pax_beta/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.rafaelpaiement.pax_beta;
+package com.example.pax_beta;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import java.math.BigDecimal;
 import java.util.EnumSet;
@@ -15,7 +14,6 @@ import io.mpos.accessories.AccessoryFamily;
 import io.mpos.accessories.parameters.AccessoryParameters;
 import io.mpos.provider.ProviderMode;
 import io.mpos.transactions.Currency;
-import io.mpos.transactions.Transaction;
 import io.mpos.transactions.parameters.TransactionParameters;
 import io.mpos.ui.shared.MposUi;
 import io.mpos.ui.shared.model.MposUiConfiguration;
@@ -24,9 +22,8 @@ public class MainActivity extends AppCompatActivity {
 
     private final static String merchantIdentifier = "1f9a9a56-6347-4e21-81d0-f59a7bcaa0a8";
     private final static String merchantSecretKey = "r5whofE1sLY9GaiPbl0uzG7oqU0xJ2eU";
-   // private final static String merchantIdentifier = "2c564e34-9b7f-4490-abf4-888a7806a8d9";
-    //private final static String merchantSecretKey = "wX07YEQ8IUkBPvFgTgTgMsRx828DY4T5";
 
+    private final static ProviderMode mode = ProviderMode.TEST;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,21 +36,10 @@ public class MainActivity extends AppCompatActivity {
                 paymentButtonClicked();
             }
         });
-        findViewById(R.id.refund).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
     }
 
-
     void paymentButtonClicked() {
-
-
-
-
-        MposUi ui = MposUi.initialize(this, ProviderMode.LIVE, merchantIdentifier, merchantSecretKey);
+        MposUi ui = MposUi.initialize(this, mode, merchantIdentifier, merchantSecretKey);
         EditText Payment = findViewById(R.id.amount);
 
         ui.getConfiguration().setSummaryFeatures(EnumSet.of(
